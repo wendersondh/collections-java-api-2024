7d6eb2cf89a906b3abeb38e3899d03ea595ef230
wenderson dhomini <wendersondhomini125@gmail.com>
2024-06-02 22:29:12 -0300

    Resolvendo desafios de OperacoesBasicas em Map
    Signed-off-by: wenderson dhomini <wendersondhomini125@gmail.com>

---
+++ src/map/OperacoesBasicas/Dicionario.java
@@ -0,0 +1,58 @@
+package map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dicionario {
+    private Map<String, String> dicionarioMap;
+
+    public Dicionario() {
+        this.dicionarioMap = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, String definicao){
+        dicionarioMap.put(palavra, definicao);
+    }
+
+    public void removerPalavra(String palavra){
+        if(!dicionarioMap.isEmpty()){
+            dicionarioMap.remove(palavra);
+        }
+    }
+
+    public void exibirPalavras(){
+        System.out.println(dicionarioMap);
+    }
+
+    public String pesquisarPorPalavra(String palavra){
+        String definicaoPorPalavra = null;
+        if(!dicionarioMap.isEmpty()){
+            definicaoPorPalavra = dicionarioMap.get(palavra);
+        }
+        return definicaoPorPalavra;
+    }
+
+    public static void main(String[] args) {
+        Dicionario dicionario = new Dicionario();
+
+        // Adicionar palavras (linguagens de programação)
+        dicionario.adicionarPalavra("java", "Linguagem de programação orientada a objetos.");
+        dicionario.adicionarPalavra("typescript", "Superset da linguagem JavaScript que adiciona tipagem estática.");
+        dicionario.adicionarPalavra("kotlin", "Linguagem moderna de programação para a JVM.");
+
+        // Exibir todas as palavras
+        dicionario.exibirPalavras();
+
+        // Pesquisar palavras
+        String definicaoJava = dicionario.pesquisarPorPalavra("java");
+        System.out.println("Definição da linguagem 'java': " + definicaoJava);
+
+        String definicaoCSharp = dicionario.pesquisarPorPalavra("csharp");
+        System.out.println(definicaoCSharp);
+
+        // Remover uma palavra
+        dicionario.removerPalavra("typescript");
+        dicionario.exibirPalavras();
+
+    }
+}
