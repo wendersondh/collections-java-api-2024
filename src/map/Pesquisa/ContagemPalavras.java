709236e57ad23959b7ee6a0aba54e4bd8a175f39
wenderson dhomini <wendersondhomini125@gmail.com>
2024-06-03 17:32:57 -0300

    Resolvendo desafios de Pesquisa em Map
    Signed-off-by: wenderson dhomini <wendersondhomini125@gmail.com>

---
+++ src/map/Pesquisa/ContagemPalavras.java
@@ -0,0 +1,55 @@
+package map.Pesquisa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContagemPalavras {
+    private Map<String, Integer> contagemPalavrasMap;
+
+    public ContagemPalavras() {
+        this.contagemPalavrasMap = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, Integer contagem){
+        contagemPalavrasMap.put(palavra, contagem);
+    }
+
+    public void removerPalavra(String palavra){
+        if(!contagemPalavrasMap.isEmpty()){
+            contagemPalavrasMap.remove(palavra);
+        }
+    }
+
+    public void exibirContagemPalavras(){
+        System.out.println(contagemPalavrasMap);
+    }
+
+    public String encontrarPalavrasMaisFrequente() {
+        String linguagemMaisFrequente = null;
+        int maiorContagem = 0;
+        for (Map.Entry<String, Integer> entry : contagemPalavrasMap.entrySet()) {
+            if (entry.getValue() > maiorContagem) {
+                maiorContagem = entry.getValue();
+                linguagemMaisFrequente = entry.getKey();
+            }
+        }
+        return linguagemMaisFrequente;
+    }
+
+    public static void main(String[] args) {
+        ContagemPalavras contagemLinguagens = new ContagemPalavras();
+
+        // Adiciona linguagens e suas contagens
+        contagemLinguagens.adicionarPalavra("Java", 2);
+        contagemLinguagens.adicionarPalavra("Python", 8);
+        contagemLinguagens.adicionarPalavra("JavaScript", 1);
+        contagemLinguagens.adicionarPalavra("C#", 6);
+
+        //  Exibe todas as palavras e suas respectivas contagens.
+        contagemLinguagens.exibirContagemPalavras();
+
+        // Encontra e exibe a linguagem mais frequente
+        String linguagemMaisFrequente = contagemLinguagens.encontrarPalavrasMaisFrequente();
+        System.out.println("A linguagem mais frequente é: " + linguagemMaisFrequente);
+    }
+}
